76c5bfceedfa2871fe89a0eae65a578ef6968f06
omar-khaled67 <ok672005@gmail.com>
2025-10-25 12:57:13 +0300

    your message

--- src/StudentDatabase.java
+++ src/StudentDatabase.java
@@ -18,8 +18,47 @@ public class StudentDatabase {
     }
 
 
-    
-    
-    
+    public void readFromFile() throws Exception
+    {
+        File file = new File(filename);
+        if (!file.exists()) {
+            System.out.println("File not found!");
+            return;
+        }
+        try
+        {
+            FileReader fr = new FileReader(file);
+            BufferedReader br = new BufferedReader(fr);
+            String line = br.readLine();
+            records.clear();
+            while (line != null) {
+                records.add(createRecordFrom(line));
+                
+                line = br.readLine();
+            }
+            br.close();
+        }
+        catch (FileNotFoundException e)
+        {
+            System.out.println("File not found!");
+        }
+
+    }
+
+
+    public Student SearchStudent(int ID)
+    {
+        
+        for(int i=0;i<records.size();i++)
+        {
+            if((records.get(i)).getStudentID()==ID)
+            {
+                return records.get(i);
+            }
+        }
+        return null;
+    }
 }
 
+    
+     
